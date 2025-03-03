936095c953b6e4b32a42ada9c25b0958d0a2da7b
AnandvPrajapati <anandvp9898@gmail.com>
2025-03-03 23:24:28 +0530

    URL correction

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,8 +1,8 @@
 export const USER_API_END_POINT =
   "https://job-hunt-portal-ap.vercel.app/api/v1/user";
 export const JOB_API_END_POINT =
-  "https://job-hunt-portal-ap.vercel.app//api/v1/job";
+  "https://job-hunt-portal-ap.vercel.app/api/v1/job";
 export const APPLICATION_API_END_POINT =
-  "https://job-hunt-portal-ap.vercel.app//api/v1/application";
+  "https://job-hunt-portal-ap.vercel.app/api/v1/application";
 export const COMPANY_API_END_POINT =
-  "https://job-hunt-portal-ap.vercel.app//api/v1/company";
+  "https://job-hunt-portal-ap.vercel.app/api/v1/company";
